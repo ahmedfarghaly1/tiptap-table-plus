c595bf04af6f91a563a0fbd80a0568d0c407a5bb
ahmed farghaly <ahmedfarghaly@ahmeds-MacBook-Pro.local>
2025-12-30 21:22:10 +0300

    remove grid border

--- src/pagination/TableCellPlus.ts
+++ src/pagination/TableCellPlus.ts
@@ -3,10 +3,7 @@ import TableCell from "@tiptap/extension-table-cell";
 export const TableCellPlus = TableCell.extend({
     addNodeView() {
         return ({ node }) => {
-          const tableNode = this.editor.extensionManager.extensions.find((extension) => extension.name === "table");
-          const borderColor = tableNode ? tableNode.options.borderColor : "black";
           const dom = document.createElement('td');
-          dom.style.border = `1px solid ${borderColor}`;
           let colspan = node.attrs.colspan;
           let rowspan = node.attrs.rowspan;
           const updateGrid = (colspan: number, rowspan: number) => {
@@ -14,13 +11,13 @@ export const TableCellPlus = TableCell.extend({
             dom.rowSpan = rowspan || 1;
             dom.setAttribute("colspan", `${colspan || 1}`);
           };
-      
+
           updateGrid(colspan, rowspan);
-      
+
           return {
             dom,
             contentDOM :  dom,
-      
+
             update(updatedNode) {
               if (updatedNode.type.name !== 'tableCell') {
                 return false;
